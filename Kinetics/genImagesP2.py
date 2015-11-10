8cd23cd713c904a612809d5050606693ea50e639
donkimber <donkimber@gmail.com>
2015-11-09 22:23:28 -0800

    playing with particles

--- Kinetics/genImagesP2.py
+++ Kinetics/genImagesP2.py
@@ -14,24 +14,30 @@ def verifyDir(path):
        print "Creating", path
        os.mkdir(path)
 
+def genImagePow2(path, opath, ow=None, oh=None):
+    if not (path.endswith(".jpg") or path.endswith(".png")):
+        return
+    im = Image.open(path)
+    w,h = im.size
+    if not ow:
+        ow = truncDown(w)
+    if not oh:
+        oh = truncDown(h)
+    size = im.size
+    im = im.resize((ow,oh), Image.ANTIALIAS)
+    print path, w, h, ow, oh
+    im.save(opath)
+
 def genImagesPow2(inputDir, outputDir):
     verifyDir(outputDir)
     names = os.listdir(inputDir)
     for name in names:
         path = os.path.join(inputDir, name)
         opath = os.path.join(outputDir, name)
-        if not path.endswith(".jpg"):
-            continue
-        im = Image.open(path)
-        w,h = im.size
-        ow = truncDown(w)
-        oh = truncDown(h)
-        size = im.size
-        im = im.resize((ow,oh), Image.ANTIALIAS)
-        print name, w, h, ow, oh
-        im.save(opath)
+        genImagePow2(path, opath)
 
 
 if __name__ == '__main__':
-    genImagesPow2("../images", "../imagesPow2")
+#    genImagesPow2("../images", "../imagesPow2")
+    genImagePow2("images/clouds.png", "images/cloudsP2.png", 128,128)
 
