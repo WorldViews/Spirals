2a5cdffaf1fa0ba9d8f257092292b4240def7555
donkimber <donkimber@gmail.com>
2015-11-10 17:14:36 -0800

    Added simple Hurricane simulation

--- Kinetics/js/CLOUDS.js
+++ Kinetics/js/CLOUDS.js
@@ -1,26 +1,37 @@
 
 CLOUDS = {};
+
 CLOUDS.Clouds = function(scene, pos, spread)
 {
     pos = pos || new THREE.Vector3();
     spread = spread || 1000;
-    var geometry = new THREE.Geometry();
 
+    var ngroups = 10;
+    var nparts = 1000;
+
+    var geometry = new THREE.Geometry();
     var sprite = THREE.ImageUtils.loadTexture( "textures/sprites/clouds.png" );
 
-    var nparts = 200;
-    var spread = 1000;
     for ( var i = 0; i < nparts; i ++ ) {
 	var vertex = new THREE.Vector3();
+	/*
 	vertex.x = pos.x + Math.random() * spread - spread/2;
 	vertex.y = pos.y + Math.random() * spread - spread/2;
 	vertex.z = pos.z + Math.random() * spread - spread/2;
+	*/
+	vertex.x = Math.random() * spread - spread/2;
+	vertex.y = Math.random() * spread - spread/2;
+	vertex.z = Math.random() * spread - spread/2;
 	geometry.vertices.push( vertex );
     }
 
-    var ngroups = 5;
     var materials = [];
     this.group = new THREE.Object3D();
+    this.group.scale.y = 0.2;
+    this.group.scale.x = 5;
+    this.group.scale.z = 5;
+    //this.group.position.copy(pos);
+    this.group.position.copy(pos);
     for ( i = 0; i < ngroups; i ++ ) {
 	color  = [1.0, 0.2, 0.5];
         size = 350;
@@ -41,13 +52,21 @@ CLOUDS.Clouds = function(scene, pos, spread)
     }
 
     this.update = function() {
-	var time = Date.now() * 0.00005;
+	var t = Date.now()/1000;
+	if (!this.prevT)
+	    this.prevT = t;
+	var dt = t - this.prevT;
+	this.prevT = t;
+	var da = 0.1*dt;
+	//report("dt: "+dt+"  da: "+da);
+        this.group.rotation.y += da;
+	da = dt;
 	var children = this.group.children;
 	for ( i = 0; i < children.length; i ++ ) {
 	    var object = children[ i ];
 	    if ( object instanceof THREE.PointCloud ) {
 		//object.rotation.y = time * ( i < 4 ? i + 1 : - ( i + 1 ) );
-		object.rotation.z = time * ( i < 4 ? i + 1 : - ( i + 1 ) );
+		object.rotation.z = da * ( i < 4 ? i + 1 : - ( i + 1 ) );
 	    }
 	}
     }
---
+++ Kinetics/js/HURRICANE.js
@@ -0,0 +1,94 @@
+
+HURRICANE = {};
+
+HURRICANE.Hurricane = function(scene, pos, spread)
+{
+    pos = pos || new THREE.Vector3();
+    spread = spread || 60;
+    this.rotSpeed = 0.1;
+    this.spread1 = 1.0*spread;
+    this.spread0 = 1.5*spread;
+    this.rMin = 120;
+    this.rMax = 1000;
+    this.narms = 8;
+    this.spriteSize = 2000;
+    this.nparts = 300;
+    this.turnsPerArm = 1;
+
+    this.init = function() {
+	color  = [1.0, 0.2, 0.5];
+
+	var sprite = THREE.ImageUtils.loadTexture( "textures/sprites/clouds.png" );
+	material = new THREE.PointCloudMaterial(
+				 { size: this.spriteSize,
+				   map: sprite,
+				   blending: THREE.AdditiveBlending,
+                                   depthTest: false,
+                                   transparent : true } );
+	material.color.setHSL( color[0], color[1], color[2] );
+	material.opacity = 0.02;
+
+        /*
+        for ( var i = 0; i < nparts; i ++ ) {
+	*/
+	this.group = new THREE.Object3D();
+	this.group.scale.y = 0.2;
+	this.group.scale.x = 5;
+	this.group.scale.z = 5;
+	this.group.position.copy(pos);
+
+	for (var i=0; i<this.narms; i++) {
+	    var a0 = 2*Math.PI*i/this.narms;
+	    arm = this.genArm(a0);
+	    var particles = new THREE.PointCloud( arm, material );
+	    this.group.add(particles);
+	}
+    }
+
+    this.genArm = function(a0) {
+	var arm = new THREE.Geometry();
+	var w = this.turnsPerArm*2*Math.PI;
+	for ( var i = 0; i < this.nparts; i ++ ) {
+	    var t = i/this.nparts;
+	    //t = t*t;            // Doing this puts more clouds towards outside.
+	    //t = Math.sqrt(t); // Doing this puts more clouds towards center.
+	    var a = w*t;
+	    var r = this.rMin*(t) + this.rMax*(1-t);
+	    var spread = this.spread0*(1-t) + this.spread1*t;
+	    var x0 = r*Math.cos(a0+a);
+	    var z0 = r*Math.sin(a0+a);
+	    //report("t: "+t+"  a: "+a+"  r: "+r);
+	    var vertex = new THREE.Vector3();
+	    vertex.x = x0 + Math.random() * spread - spread/2;
+	    vertex.y =      Math.random() * spread - spread/2;
+	    vertex.z = z0 + Math.random() * spread - spread/2;
+	    report("v "+JSON.stringify(vertex));
+	    arm.vertices.push( vertex );
+	}
+	return arm;
+    };
+
+    this.update = function() {
+	var t = Date.now()/1000;
+	if (!this.prevT)
+	    this.prevT = t;
+	var dt = t - this.prevT;
+	this.prevT = t;
+	var da = -this.rotSpeed*dt;
+	//report("dt: "+dt+"  da: "+da);
+        this.group.rotation.y += da;
+	da = dt;
+	/*
+	var children = this.group.children;
+	for ( i = 0; i < children.length; i ++ ) {
+	    var object = children[ i ];
+	    if ( object instanceof THREE.PointCloud ) {
+		//object.rotation.y = time * ( i < 4 ? i + 1 : - ( i + 1 ) );
+		object.rotation.z = da * ( i < 4 ? i + 1 : - ( i + 1 ) );
+	    }
+	}
+	*/
+    };
+
+    this.init();
+}
