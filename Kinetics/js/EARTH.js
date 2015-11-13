c9c07211cd1da0733a1423448473e74f5b92f5c6
donkimber <donkimber@gmail.com>
2015-11-12 19:07:41 -0800

    localGroups on earth kind of working

--- Kinetics/js/EARTH.js
+++ Kinetics/js/EARTH.js
@@ -38,9 +38,12 @@ EARTH.Earth = function(group, radius)
 	return EARTH.latLonToVector3(lat, lng, this.radius, h);
     }
 
-    this.getLocalGroup = function(lat, lng, h) {
+    this.getLocalGroup = function(lat, lon, h) {
 	var localGroup = new THREE.Group();
-	localGroup.position.copy(this.latLonToVector3(lat, lng, h));
+	localGroup.position.copy(this.latLonToVector3(lat, lon, h));
+	var phi = (90-lat)*Math.PI/180;
+	var theta = (lon+90)*Math.PI/180;
+	localGroup.rotation.set(phi, theta, 0, "YXZ");
 	this.group.add(localGroup);
 	return localGroup;
     }
@@ -51,13 +54,12 @@ EARTH.Earth = function(group, radius)
         var material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
         var marker = new THREE.Mesh( geometry, material );
 	var lg = this.getLocalGroup(lat, lon, h);
+	/*
 	var phi = (90-lat)*Math.PI/180;
-	var theta = (lon-180)*Math.PI/180;
+	var theta = (lon+90)*Math.PI/180;
+	lg.rotation.set(phi, theta, 0, "YXZ");
+	*/
 	lg.add(marker);
-	lg.rotation.set(phi, theta, 0, "XYZ");
-	//lg.rotation.set(theta, phi, 0, "YXZ");
-	//	lg.rotation.x = -phi;
-	//	lg.rotation.y = theta;
 	var axisHelper = new THREE.AxisHelper( 10*h );
 	lg.add( axisHelper );
 	return lg;
