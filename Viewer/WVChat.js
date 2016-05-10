494503196b85be34deb813f3e4e34850f2ccd952
donkimber <donkimber@gmail.com>
2016-05-10 11:28:26 -0700

    improved and cleaned up chat
    Scroll now works, and it is dragable by a kind of title bar

--- Viewer/WVChat.js
+++ Viewer/WVChat.js
@@ -28,8 +28,8 @@ WV.handleChatData = function(data, name)
         var rec = recs[i];
 	report("chat rec: "+JSON.stringify(rec));
 	var str = rec.name+": "+rec.text;
-	$("#chatDisplay").append(str+"<br>");
-	//$("#chatDisplay").prepend(str+"<br>");
+	//$("#chatText").append(str+"<br>");
+	$("#chatText").prepend(str+"<br>");
 	if (rec.id == WV.myId) {
 	    report("******** SKIPPING MY OWN RECORD *********");
 	    continue;
@@ -55,25 +55,25 @@ WV.setChatVisibility = function(v)
 {
     var layer = WV.layers["chat"];
     if (v) {
-	report("Show #chatView ");
-	$("#chatView").show();
+	report("Show #chatWindow ");
+	$("#chatWindow").show();
 	if (!WV.watchingChat)
 	    WV.postChatMessage("[joining chat]");
 	WV.watchingChat = true;
 	if (!layer.visible)
 	layer.visible = true;
-	//$("#chatDisplay").append("<br>");
+	//$("#chatText").append("<br>");
 	/*
 	setTimeout(function() {
 		report("ughhh......");
-		$("#chatDisplay").append("<br>");
+		$("#chatText").append("<br>");
 	    }, 1000);
 	*/
     }
     else {
-	report("hide #chatView ");
+	report("hide #chatWindow ");
 	layer.visible = false;
-	$("#chatView").hide(200);
+	$("#chatWindow").hide(200);
 	if (WV.watchingChat)
 	    WV.postChatMessage("[leaving chat]");
 	WV.watchingChat = false;
@@ -119,6 +119,26 @@ $(document).ready(function()
 	    $("#chatText").val("");
             return false;
 	});
+    $('#dismissChat').click(function(e) {
+        WV.setChatVisibility(false);
+    });
+    $('#chatTitle').on('mousedown', function(e) {
+        var offs0 = $('#chatWindow').offset();
+        var p0 = {x: e.pageX, y: e.pageY};
+        report(" offset: "+offs0.top+" "+offs0.left);
+        $('#chatWindow').on('mousemove', function(e) {
+            $('#chatWindow').offset({
+                top: offs0.top + (e.pageY - p0.y),
+                left: offs0.left + (e.pageX-p0.x)
+            }).on('mouseup', function() {
+                report("mouseup");
+                //$('#chatWindow').off('mousemove', false);
+                $('#chatWindow').off('mousemove');
+            });
+        });
+        e.preventDefault();
+    });
+
 });
 
 
