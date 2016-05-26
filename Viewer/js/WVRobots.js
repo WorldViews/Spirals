2c1d68ce36426f3656f8fe092b39411743a9a54d
donkimber <donkimber@gmail.com>
2016-05-26 16:49:47 -0700

    added some sample paths for PanoBot tour

--- Viewer/js/WVRobots.js
+++ Viewer/js/WVRobots.js
@@ -53,6 +53,10 @@ WV.Robots.handleRecs = function(data, name)
 	    WV.addCoordinateSystem(csName, rec);
 	    continue;
 	}
+	if (rec.type == "robotTrail") {
+	    WV.Robots.addTrail(layer, rec);
+	    continue;
+	}
         report("rec "+i+" "+JSON.stringify(rec));
         layer.numObjs++;
 	var dt = t - rec.t;
@@ -98,6 +102,43 @@ WV.Robots.handleRecs = function(data, name)
 	}
     }
 }
+
+WV.Robots.addTrail = function(layer, rec)
+{
+    report("WV.Robots.addTrail "+layer.name);
+    var url = rec.dataUrl;
+    WV.getJSON(url, function(data) {
+	    WV.Robots.handleTrailData(layer, rec, data);
+    });
+}
+
+WV.Robots.handleTrailData = function(layer, rec, data)
+{
+    var recs = data.recs;
+    var coordSys = rec.coordSys;
+    var points = [];
+    var pathId = "robot_path_"+rec.id
+    for (var i=0; i<recs.length; i++) {
+	var tr = recs[i];
+	var pos = tr.pos;
+	var lla = WV.xyzToLla(tr.pos, coordSys);
+	report(" "+i+"  "+pos+"  "+lla);
+	points.push(Cesium.Cartesian3.fromDegrees(lla[1], lla[0], 0));
+    }
+    var material = new Cesium.PolylineGlowMaterialProperty({
+	    color : Cesium.Color.RED,
+	    glowPower : 0.15});
+    var opts = { positions : points,
+		 id: pathId,
+		 width : 3.0,
+		 material : material };
+    var route = null;
+    var polylines = WV.getTetherPolylines();
+    route = polylines.add({polyline: opts});
+    route = route.polyline;
+    return route;
+}
+
 
 WV.Robots.handleClick = function(rec)
 {
