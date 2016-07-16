4a342cf8fe51814d0488ee70fbd692246b36cf7b
enockg <enockglidden@hotmail.com>
2016-07-15 17:36:50 -0700

    filtered gardens layer

--- scripts/filterLayer.py
+++ scripts/filterLayer.py
@@ -26,7 +26,8 @@ def filterByLand(path, opath):
 #filterByLand("../Viewer/data/temples0_data.json", "../Viewer/data/temples_data.json")
 #filterByLand("../Viewer/data/climbing0_data.json", "../Viewer/data/climbing_data.json")
 #filterByLand("../Viewer/data/temples0_data.json", "../Viewer/data/temples_data.json")
-filterByLand("../Viewer/data/hiking0_data.json", "../Viewer/data/hiking_data.json")
+#filterByLand("../Viewer/data/hiking0_data.json", "../Viewer/data/hiking_data.json")
+filterByLand("../Viewer/data/gardens0_data.json", "../Viewer/data/gardens_data.json")
 
 
 
