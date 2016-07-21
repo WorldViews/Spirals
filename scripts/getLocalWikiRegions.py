930e2bdc450a40a2bcb94126572905eba26f8fb9
donkimber <donkimber@gmail.com>
2016-07-20 20:06:34 -0700

    worked some on script for getting local wiki layers

--- scripts/getLocalWikiRegions.py
+++ scripts/getLocalWikiRegions.py
@@ -3,6 +3,7 @@ import json
 import urllib2
 
 def fetchMaps():
+    id = 0
     url = "https://localwiki.org/api/v4/regions/?format=json"
     uos = urllib2.urlopen(url)
     buf = uos.read()
@@ -12,16 +13,35 @@ def fetchMaps():
     next = obj["next"]
     print "next:", next
     results = obj["results"]
+    wvrecs = []
     for res in results:
         name = res["full_name"]
+	try:
+	    print "name:", name, "  url:", url
+	except:
+            print "Bad name"
+	    continue
 	url = res["url"]
 	try:
 	    coords = res["settings"]["region_center"]["coordinates"]
 	except:
             print "Unable to get coordinates"
 	    continue
-	print "name:", name, "  url:", url
+	id += 1
 	print "coords:", coords
 	print
+	wvrec = {'id': "%d" % id,
+		 'lat': coords[1],
+		 'lon': coords[0],
+		 'title': name,
+		 'url': url}
+	wvrecs.append(wvrec)
+	print wvrec
+    #print "wvrecs", wvrecs
+    layer = {}
+    layer["name"] = "LocalWiki Regions"
+    layer["records"] = wvrecs
+    print layer
+    json.dump(layer, file("localWikiRegions_data.json", "w"), indent=4)
 
 fetchMaps()
